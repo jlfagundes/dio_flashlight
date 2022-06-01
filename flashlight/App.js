3ae3327e4d705f550757c1d6de2cb2122eadbfc8
jlfagundes <jlfagundes@gmail.com>
2022-05-31 22:01:27 -0300

    feat: toggle background

--- flashlight/App.js
+++ flashlight/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import {View, StyleSheet} from 'react-native';
 
-const App = () => {};
+const App = () => {
+  const toggle = true;
+
+  return <View style={!toggle ? style.container : style.containerLight} />;
+};
 
 export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
