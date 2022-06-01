2797a857e33d47d5cbbe9e491bb22ccf9e2fa32a
jlfagundes <jlfagundes@gmail.com>
2022-06-01 15:46:00 -0300

    feat: insert images

--- flashlight/App.js
+++ flashlight/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 
 const App = () => {
-  const toggle = true;
+  const toggle = false;
 
-  return <View style={!toggle ? style.container : style.containerLight} />;
+  return (
+    <View style={toggle ? style.containerLight : style.container}>
+      <Image
+        style={toggle ? style.lightinigOn : style.lightinigOff}
+        source={
+          toggle
+            ? require('./assets/icons/eco-light.png')
+            : require('./assets/icons/eco-light-off.png')
+        }
+      />
+      <Image
+        style={style.dioLogo}
+        source={
+          toggle
+            ? require('./assets/icons/logo-dio.png')
+            : require('./assets/icons/logo-dio-white.png')
+        }
+      />
+    </View>
+  );
 };
 
 export default App;
@@ -21,5 +40,24 @@ const style = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  lightinigOn: {
+    resizeMode: 'contain', // mudando o tamanho fica no espaço resevado(container)
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightinigOff: {
+    resizeMode: 'contain', // mudando o tamanho fica no espaço resevado(container)
+    alignSelf: 'center',
+    tintColor: 'white', // consegue trocar a cor da imagem
+    width: 150,
+    height: 150,
+  },
+  dioLogo: {
+    resizeMode: 'contain', // mudando o tamanho fica no espaço resevado(container)
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
   },
 });
